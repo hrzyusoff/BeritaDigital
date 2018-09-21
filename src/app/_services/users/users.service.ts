adcbbd8b66b15fdf8e4b30a7f0c01c870d504636
hrzyusoff <hrzyusoff@gmail.com>
2018-09-21 09:59:21 +0800

    fix catch error

--- src/app/_services/users/users.service.ts
+++ src/app/_services/users/users.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { fakeusers } from './users';
-// import { Observable, throwError } from 'rxjs';
-// import { catchError } from 'rxjs/operators';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError as observableThrowError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 
@@ -12,15 +10,13 @@ import { catchError } from 'rxjs/operators';
 })
 export class UsersService {
 
-  private url: string = 'https://jsonplaceholder.typicode.com/posts';
-
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<fakeusers[]> {
-    return this.http.get<fakeusers[]>(this.url).pipe(catchError(this.errorHandler))
+    return this.http.get<fakeusers[]>('https://jsonplaceholder.typicode.com/posts').pipe(catchError(this.errorHandler))
   }
 
   errorHandler(error: HttpErrorResponse) {
-    return Observable.pipe(throwError(error.message || "Ops it is an error !"))
+    return observableThrowError(error.message || "Ops it is an error !")
   }
 }
